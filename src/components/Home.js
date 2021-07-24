787ba85a37c81724339367d389917ef6a15e5de7
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-07-23 23:16:07 -0300

    adiciona carrossel e estiliza home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel'
+import '../styles/Home.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <Link to='/game'>Play</Link>
-        <Link to='/about'>About</Link>
+        <header className='header-home'>
+        <div className='div-header'>
+          <Link to='/about' className='btn-about'>About</Link>
+        </div>
+        </header>
+        <section className='section-carousel'>
+          <Carousel autoPlay='true'>
+            <div>
+              <p>1</p>
+            </div>
+            <div>
+              <p>2</p>
+            </div>
+            <div>
+              <p>3</p>
+            </div>
+            <div>
+              <p>4</p>
+            </div>
+            <div>
+              <p>5</p>
+            </div>
+          </Carousel>
+        </section>
+        <section className='section-play'>
+          <div className='div-section-play'>
+            <Link to='/game' className='btn-play'>PLAY</Link>
+          </div>
+        </section>
       </div>
     )
   }
